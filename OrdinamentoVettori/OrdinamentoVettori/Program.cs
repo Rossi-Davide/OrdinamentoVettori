454bb60de3550e929344c03edb9f0d8d45e8f2f6
Davide Rossi (4I) <DAVIDE.ROSSI.4i@local.ittcesena.it>
2021-10-08 08:35:15 +0200

    Aggiunta commenti

--- OrdinamentoVettori/OrdinamentoVettori/Program.cs
+++ OrdinamentoVettori/OrdinamentoVettori/Program.cs
@@ -6,17 +6,20 @@ namespace OrdinamentoVettori
     {
         static void Main(string[] args)
         {
+            //Creazione vettore 
             int [] vettore = new int [10] { 11, 55, -9, 7, 74, 147, -87, 6, 0, 18};
 
-
+            //ordinamento vettore
             ordina(vettore);
 
+            //stampa vettore
             for (int i = 0; i < 10; i++)
                 Console.WriteLine(vettore[i]);
 
             Console.ReadKey();
         }
 
+        //metodo per ordinamento del vettore
         static void ordina (int[] vettore)
         {
             Array.Sort(vettore);
